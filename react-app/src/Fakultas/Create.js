f141dae74d830ef82a83bfba019616b61933f3a1
riss-aa <103017800+riss-aa@users.noreply.github.com>
2023-12-02 15:14:27 +0700

    1

--- react-app/src/Fakultas/Create.js
+++ react-app/src/Fakultas/Create.js
@@ -1,12 +1,36 @@
+import { Axios } from "axios"
 import React, { useState } from "react"
-const create = () => {
+import { Navigate, useNavigate } from "react-router-dom"
+const Create = () => {
+    const Navigate = useNavigate()
     const [fakultas, setFakultas] = useState({
         nama: ""
     })
+
+    const handleChange = (e, name) => {
+        const value = e.target.value
+        setFakultas({...fakultas,[name]: value})
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        try{
+            Axios.post("https://apimi5a.vercel.app/fakultas", fakultas)
+            .then ((res) => {
+                alert('Fakultas berhasil disimpan') 
+                Navigate("/fakultas")
+            })
+        } catch(error){
+            alert(error)
+        }
+    }
     return(
     <>
         <h2>Halaman Create Fakultas</h2>
+        <form>
+            <input type="text" value={fakultas.nama} onChange={(e) => handleChange(e, "nama")} className="form-control" placeholder="Input Nama Fakultas" />
+            <button onClick={handleSubmit} className="btn btn-primary">Simpan</button>
+        </form>
     </>
     )
 }
-export default create
+export default Create
